60ad025d078cfaff52ba0a419084a31b224499c0
Nanoshido <Ledezman523@gmail.com>
2020-02-10 22:11:25 -0300

    actulizado

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
 
+if (edad >= 18) {
+    alert ("adulto");
+}
+else{
+    if (edad < 13) {
+        alert("niño");
+    }
+    else{
+        alert("adolecente")
+    }
+}
 
 }//FIN DE LA FUNCIÓN
